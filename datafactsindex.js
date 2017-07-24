9f5fe24e9cbbc4bcf3956690ea02781aa3ab8253
Georgia Tardy <georgiatardyart@gmail.com>
2017-07-24 17:23:55 -0400

    completed index.js, completed project

--- datafactsindex.js
+++ datafactsindex.js
@@ -13,10 +13,23 @@ let secondsinDay = moment().format("SSSS")
 let current = moment()
 let endofDay = moment().startOf("day")
 // Daylight Savings
-let daylightSavings = moment().format()
+let daylightSavings = moment().isDST()
 // Leap Year
 let leapYear = moment
 
 console.log(`It is the ${chalk.blue.bold(now)}`);
 console.log(`It is the ${chalk.rgb(239, 64, 207).bold(dayofYear) + chalk.rgb(239, 64, 207).bold('th')} day of the year.`);
 console.log(`It is ${chalk.rgb(99, 235, 242).bold(parseInt((current - endofDay) / 1000))} seconds into the day`);
+
+// These if/else statements are for daylight saving and leap year
+
+if (daylightSavings === true) {
+    console.log(`It ${chalk.rgb(142, 168, 99).bold('is')} during Daylight Savings Time`)
+} else {
+    console.log(`It ${chalk.rgb(174, 63, 68).bold('is not')} during Daylight Savings Time`)
+}
+if (leapYear === true) {
+    console.log(`It ${chalk.rgb(142, 168, 99).bold('is')} a leap year.`)
+} else {
+    console.log(`It ${chalk.rgb(174, 63, 68).bold('is not')} a leap year.`)
+}
