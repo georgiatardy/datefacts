a6563502cf7555f3b9fd9054b2b0dcba4a45ea87
Georgia Tardy <georgiatardyart@gmail.com>
2017-07-24 17:05:32 -0400

    added seconds into day

--- datafactsindex.js
+++ datafactsindex.js
@@ -9,7 +9,9 @@ let now = moment().format("dddd, MMMM Do YYYY, HH:mm:ss a");
 // Number of day in year
 let dayofYear = moment().format("DDDD");
 // Seconds in day
-let secondsinDay = moment().format()
+let secondsinDay = moment().format("SSSS")
+let current = moment()
+let endofDay = moment().startOf("day")
 // Daylight Savings
 let daylightSavings = moment().format()
 // Leap Year
@@ -17,3 +19,4 @@ let leapYear = moment
 
 console.log(`It is the ${chalk.blue.bold(now)}`);
 console.log(`It is the ${chalk.rgb(239, 64, 207).bold(dayofYear) + chalk.rgb(239, 64, 207).bold('th')} day of the year.`);
+console.log(`It is ${chalk.rgb(99, 235, 242).bold(parseInt((current - endofDay) / 1000))} seconds into the day`);
